5c7af0e1267a616b97cb59aec78231293da71ebf
rettt767 <3098923672@qq.com>
2024-06-04 08:29:30 +0800

    创建仓库

---
+++ src/main/java/ltd/jellyfish/App.java
@@ -0,0 +1,13 @@
+package ltd.jellyfish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
